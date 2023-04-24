637efddf1e4d9fe2b031c081ba9ea45df25ad6e0
namlp1908 <namlp1908@gmail.com>
2023-04-24 10:26:45 +0700

    delete Output folder

--- CHT/ViewModel/MainViewModel.cs
+++ CHT/ViewModel/MainViewModel.cs
@@ -1,28 +1,82 @@
-﻿using System;
+﻿using CHT.Command.Cmd;
+using CHT.Commons;
+using CHT.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+using Kis.Toolkit;
 
 namespace CHT.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
-        private static MainViewModel _instance = null;
-        public static MainViewModel GetInstance
+        private static Dispatcher _dispatcher;
+        public MainView MainView { get; private set; }
+        //private static MainViewModel _instance = null;
+        //public static MainViewModel GetInstance
+        //{
+        //    get
+        //    {
+        //        if(_instance == null)
+        //        {
+        //            _instance = new MainViewModel(_dispatcher);
+        //        }
+        //        return _instance;
+        //    }
+        //}
+        public static MainViewModel Instance { get; private set; }
+        
+        #region ViewModels
+        public WeighViewModel WeighViewModel { get; private set; }
+        #endregion
+        public MainViewModel(Dispatcher dispatcher, MainView mainView, WeighViewModel weighViewModel) 
         {
-            get
+            if (Instance is null)
             {
-                if(_instance == null)
-                {
-                    _instance = new MainViewModel();
-                }
-                return _instance;
+                Instance = this;
+            }
+            else
+                return;
+            _dispatcher = dispatcher;
+            MainView = mainView;
+            WeighViewModel = weighViewModel;
+         
+            this.OpenCOM = new OpenCOMCmd(this);
+            this.PressEnterKey = new PressEnterKeyCmd(this);
+        }
+        public void WirteTest(Paragraph para, string text)
+        {
+            para.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                para.Inlines.Add(text);
+            }));
+        }
+        public void ShowData(string data)
+        {
+            MainView.tbWeigh.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                MainView.tbWeigh.Text = data;
+            }));
+        }
+        private double _opacityMain = 1.0;
+        public double OpacityMain
+        {
+            get { return _opacityMain; }
+            set
+            {
+                SetProperty(ref _opacityMain, value);
             }
         }
-        public MainViewModel() 
-        { 
-        }
-        ~MainViewModel() { }
+        public ICommand Refesh { get; }
+        public ICommand OpenCOM { get; }
+        public ICommand CloseCOM { get; }
+        public ICommand PressEnterKey { get; }
     }
 }
